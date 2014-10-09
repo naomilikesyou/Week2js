9edb68d0e4099233df64396e933d418b9d0443de
naomilikesyou <ntewodros09@gmail.com>
2014-10-08 20:04:13 -0400

    hide/show button disclaimer

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
    console.log("OMG JAVASCRIPT");
    $("#celebs tbody tr:even").addClass("zebra")
+
+   $('#hideButton').click(function(){
+   		$('#disclaimer').hide();
+   })
+   $('#showButton').click(function(){
+   		$('#disclaimer').show();
+   })
 });
