f8ff0d128da62782b6b63326ce17ae27c3d23b51
naomilikesyou <ntewodros09@gmail.com>
2014-10-08 20:33:59 -0400

    Removed hide/show fixed and instated toggle
    Fixed .js

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
    console.log("OMG JAVASCRIPT");
    $("#celebs tbody tr:even").addClass("zebra")
 
-   $('#toggleButton').click(function(){
-   		if ($('#disclaimer').is(':visible')){
-   			$('#disclaimer').hide();
-   		}
-
-   		else {
-   			$('#disclaimer').show();
-   		}
-   })
+   // $('#toggleButton').click(function(){
+   // 		if ($('#disclaimer').is(':visible')){
+   // 			$('#disclaimer').hide();
+   // 		}
+   // 		else {
+   // 			$('#disclaimer').show();
+   // 		}
+   // })
+// 
+   		$('#toggleButton').click(function(){
+   			$('#disclaimer').toggle();
+   		})
 });
